1c18dc3a172a5b66c56fd54e52b3d27a77b8e8fa
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-25 20:31:15 -0300

    fix: remove duplicate skeleton viewer declaration

--- viewer/include/auto_animation/viewer/Viewer.hpp
+++ viewer/include/auto_animation/viewer/Viewer.hpp
@@ -35,7 +35,6 @@ public:
     void run_for_frames(std::uint64_t frame_count);
     void request_close();
     void set_skeleton(const rigging::Skeleton* skeleton);
-    void set_skeleton(const rigging::Skeleton* skeleton);
 
     [[nodiscard]] bool initialized() const noexcept;
     [[nodiscard]] const ViewerStats& stats() const noexcept;
